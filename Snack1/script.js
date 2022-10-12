f92f47f7bd2b76ac2b6ab42cd17e7ce34df1e144
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-12 11:50:38 +0200

    Final Commit

---
+++ Snack1/script.js
@@ -0,0 +1,12 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+// Usare prompt per inserire due numeri in successione
+const num1 = Number (prompt('Scrivi il primo numero'));
+const num2 = Number (prompt('Scrivi il secondo numero'));
+// Verifica il valore maggiore tra i due e stampalo su console
+if (num1 > num2) {
+    console.log(num1).value;
+} else {
+    console.log(num2).value;
+}
