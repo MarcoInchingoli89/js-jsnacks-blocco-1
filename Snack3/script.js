a049a26b0077e2f23e322cc3399e4947731d6076
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-12 12:32:18 +0200

    Add Snack3

---
+++ Snack3/script.js
@@ -0,0 +1,18 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.  */
+
+// Chiedo con prompt di inserire dieci numeri
+const num1 = Number (prompt('Inserisci un numero'));
+const num2 = Number (prompt('Inserisci un numero'));
+const num3 = Number (prompt('Inserisci un numero'));
+const num4 = Number (prompt('Inserisci un numero'));
+const num5 = Number (prompt('Inserisci un numero'));
+const num6 = Number (prompt('Inserisci un numero'));
+const num7 = Number (prompt('Inserisci un numero'));
+const num8 = Number (prompt('Inserisci un numero'));
+const num9 = Number (prompt('Inserisci un numero'));
+const num10 = Number (prompt('Inserisci un numero'));
+
+// Stampa la somma di tutti i numeri
+const sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log(sum);
