823eb1c61efb34a0b69f476fd25374c9b6551335
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-13 12:53:01 +0200

    Add Snack 4

---
+++ Snack4/script.js
@@ -0,0 +1,50 @@
+/* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+ */
+
+//Creo un array
+const vipList = ['James Bond, Topolino, Frodo, Gandalf, Neo'];
+
+// Creo una variabile di uscita
+let inOut = ("Mi dispiace, ma non sei nella lista");
+// Metto all'esterno la variabile i per il ciclo while
+let i=0;
+do {
+  // Chiedo all'utente se è stato invitato usando prompt
+  const vipListCheck = prompt('Benvenuto alla festa del grande Gatsby, posso sapere il tuo nome?')
+  // Con il While verifico la risposta
+} while (i<0) {
+  inOut = "Sei nella lista, divertiti alla festa";
+}
+
+// Stampo la risposta in console
+console.log(inOut);
+
+
+
+
+
+
+// Chiedo con prompt di inserire dieci numeri
+/* let sum = 0;
+for (let index = 0; index < 10; index++) {
+  const userNumber = Number(prompt('Type a number'))
+  sum += userNumber
+} */
+
+// Stampa la somma di tutti i numeri
+/* console.log(sum); */
+
+/* Ciclo While */
+
+// Chiedo con prompt di inserire dieci numeri
+/* let sum = 0;
+let i=0;
+while (i < 10) {
+  const userNumber = Number(prompt('Type a number'))
+  sum += userNumber
+  i++
+  
+} */
+
+// Stampa la somma di tutti i numeri
+/* console.log(sum); */
